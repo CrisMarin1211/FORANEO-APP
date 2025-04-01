18dcf1a36ddb0f2a2c6cf5680d0c78e2157ba753
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 11:36:56 -0500

    feat: Add basic structure for PlanYetPhrase component

---
+++ Foraneo-App/src/Recipes/Components/PlanYetPhrase/PlanYetPhrase.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PlanYetPhrase.css';
+
+function PlanYet() {
+	return (
+		<div className='Phrase'>
+			<p>You don't have a plan yet</p>
+		</div>
+	);
+}
+
+export default PlanYet;
