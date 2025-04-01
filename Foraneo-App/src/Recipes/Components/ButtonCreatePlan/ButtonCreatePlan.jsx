f08ba10a127ba0081a2744c18e9c70f702b1654c
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 11:42:32 -0500

    feat: Add basic structure for ButtonCreatePlan component

---
+++ Foraneo-App/src/Recipes/Components/ButtonCreatePlan/ButtonCreatePlan.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonCreatePlan.css';
+
+function ButtonCreatePlan() {
+	return (
+		<div className='Button-Create'>
+			<button className='Create-Plan'> Create Plan</button>
+		</div>
+	);
+}
+
+export default ButtonCreatePlan;
