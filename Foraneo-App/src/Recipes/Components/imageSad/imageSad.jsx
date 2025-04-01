f1bb4f748740fdea39655d1c750da6c031e40a22
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 11:41:09 -0500

    feat: Add basic structure for ImageSad component

---
+++ Foraneo-App/src/Recipes/Components/imageSad/imageSad.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './imageSad.css';
+
+function ImageSad() {
+	return (
+		<div className='Image-Sad'>
+			<img src='https://pbs.twimg.com/media/GnaxxsjXYAAIIa8?format=png&name=240x240' alt='Sad Emoji' />
+		</div>
+	);
+}
+
+export default ImageSad;
