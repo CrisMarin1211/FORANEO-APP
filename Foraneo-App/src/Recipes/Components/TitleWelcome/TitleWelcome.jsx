cb06cb60d8280c014672b113ac77b2baf490390a
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 11:32:49 -0500

    feat: Add basic structure for TitleWelcome component

---
+++ Foraneo-App/src/Recipes/Components/TitleWelcome/TitleWelcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TitleWelcome.css';
+
+function Title() {
+	return (
+		<div className='Welcome'>
+			<h2 className='title-text'>Welcome to my Weekly plan!</h2>
+		</div>
+	);
+}
+
+export default Title;
