818f9d4f11740c48678da7855aefe138294368d0
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 11:46:50 -0500

    feat: Add basic structure for TitleBuild component

---
+++ Foraneo-App/src/Recipes/Components/1.CreateYourPlan/TitleBuild/TitleBuild.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TitleBuild.css';
+
+function TitleBuild() {
+	return (
+		<div className='Build'>
+			<h2 className='title-build'>Build Your Weekly Plan!</h2>
+		</div>
+	);
+}
+
+export default TitleBuild;
