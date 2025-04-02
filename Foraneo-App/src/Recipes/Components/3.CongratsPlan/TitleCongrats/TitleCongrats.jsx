dafd4c120af3785da8e53e7f5601adcde2029767
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 19:38:59 -0500

    feat: Add basic structure for TitleCongrats component

---
+++ Foraneo-App/src/Recipes/Components/3.CongratsPlan/TitleCongrats/TitleCongrats.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TitleCongrats.css';
+
+function TitleCongrats() {
+	return (
+		<div className='Congrats'>
+			<h2 className='title-Congrats'>Congratulations! 🎉</h2>
+		</div>
+	);
+}
+
+export default TitleCongrats;
