ea8f9426ce722f9e0fd806bd30da734a49b0c9cb
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 19:40:42 -0500

    feat: Add basic structure for ImageEmotions component

---
+++ Foraneo-App/src/Recipes/Components/3.CongratsPlan/ImageEmotions/ImageEmotions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ImageEmotions.css';
+
+function ImageEmotions() {
+	return (
+		<div className='Image-Emotions'>
+			<img src='https://pbs.twimg.com/media/Gne7Ts_XQAAhx3G?format=png&name=240x240' alt='Emotions Emoji' />
+		</div>
+	);
+}
+
+export default ImageEmotions;
