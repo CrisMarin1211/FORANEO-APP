6ad4ad54e82c7b09a82d0073816484ca35381c31
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 19:37:48 -0500

    feat: Add basic structure for UnlockedMessage component

---
+++ Foraneo-App/src/Recipes/Components/3.CongratsPlan/UnlockedMessage/UnlockedMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UnlockedMessage.css';
+
+function UnlockedMessage() {
+	return (
+		<div className='Unlocked-Message'>
+			<p className='message'>🔓 You have unlocked the Banana Oatmeal Bowl</p>
+		</div>
+	);
+}
+
+export default UnlockedMessage;
