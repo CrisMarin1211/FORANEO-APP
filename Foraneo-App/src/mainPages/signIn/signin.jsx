d9a60ac4f481eb7451008cd3ecc1e3d530ecc26c
Azuki19 <analucia27259@gmail.com>
2025-04-01 21:03:10 -0500

    feat: sign-in components: Initial Form

--- Foraneo-App/src/mainPages/signIn/signin.jsx
+++ Foraneo-App/src/mainPages/signIn/signin.jsx
@@ -1,48 +1,12 @@
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import SignInForm from './components/signInForm/signInForm';
 
 const SignIn = () => {
-	const navigate = useNavigate();
-
-	const onFinish = (values) => {
-		console.log('Success:', values);
-		navigate('/login'); // Redirige a /login cuando el form se envía
-	};
-
-	const onFinishFailed = (errorInfo) => {
-		console.log('Failed:', errorInfo);
-	};
-
 	return (
-		<Form
-			name='basic'
-			labelCol={{ span: 8 }}
-			wrapperCol={{ span: 16 }}
-			style={{ maxWidth: 600 }}
-			initialValues={{ remember: true }}
-			onFinish={onFinish}
-			onFinishFailed={onFinishFailed}
-			autoComplete='off'
-		>
-			<Form.Item label='Username' name='username' rules={[{ required: true, message: 'Please input your username!' }]}>
-				<Input />
-			</Form.Item>
-
-			<Form.Item label='Password' name='password' rules={[{ required: true, message: 'Please input your password!' }]}>
-				<Input.Password />
-			</Form.Item>
-
-			<Form.Item name='remember' valuePropName='checked' label={null}>
-				<Checkbox>Remember me</Checkbox>
-			</Form.Item>
-
-			<Form.Item label={null}>
-				<Button type='primary' htmlType='submit'>
-					Submit
-				</Button>
-			</Form.Item>
-		</Form>
+		<section>
+			<h1>Sign In</h1>
+			<SignInForm />
+		</section>
 	);
 };
 
