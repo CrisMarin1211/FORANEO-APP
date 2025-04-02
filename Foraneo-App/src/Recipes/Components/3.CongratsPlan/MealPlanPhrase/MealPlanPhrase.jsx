4f03b85b43e444689950fa629523a05ff772da1b
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 19:39:46 -0500

    feat: Add basic structure for MealPlanPhrase component

---
+++ Foraneo-App/src/Recipes/Components/3.CongratsPlan/MealPlanPhrase/MealPlanPhrase.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MealPlanPhrase.css';
+
+function MealPlanPhrase() {
+	return (
+		<div className='MealPlan-Phrase'>
+			<p>Now you can make it in any meal plan</p>
+		</div>
+	);
+}
+
+export default MealPlanPhrase;
