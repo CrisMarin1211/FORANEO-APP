a97c86403272ffa251235b5aa1c56085910eef1c
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 20:19:58 -0500

    feat: Add basic structure for NameRecipe component

---
+++ Foraneo-App/src/mainPages/Recipes/Components/3.CongratsPlan/NameRecipe/NameRecipe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NameRecipe.css';
+
+function NameRecipe() {
+	return (
+		<div className='Name-Recipe'>
+			<h2 className='title-NameRecipe'>Banana Oatmeal Bowl</h2>
+		</div>
+	);
+}
+
+export default NameRecipe;
