6c9a7af506daa846230a54473eef4bd0d5db308e
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 19:42:18 -0500

    feat: Add Congratulations page to App component

--- Foraneo-App/src/App.jsx
+++ Foraneo-App/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import './App.css';
-// import Welcome from './Recipes/Pages/WelcomeRecipes/Welcome';
+import Welcome from './Recipes/Pages/WelcomeRecipes/Welcome';
 import PlanCreate from './Recipes/Pages/PlanCreate/PlanCreate';
+import Congratulations from './Recipes/Pages/Congratulation/Congratulation';
 
 function App() {
 	return (
 		<div className='App'>
 			{/* <Welcome /> */}
-			<PlanCreate />
+			{/* <PlanCreate /> */}
+			<Congratulations />
 		</div>
 	);
 }
