fd6f8180025e77f134738d085d7a28a5f766e572
Azuki19 <analucia27259@gmail.com>
2025-04-01 20:53:30 -0500

    feat: sign-In form and final logic to navigate through pages

--- Foraneo-App/src/mainPages/export.jsx
+++ Foraneo-App/src/mainPages/export.jsx
@@ -1,2 +1,2 @@
 export { default as SignInView } from "./signIn/signin";
-export { default as LogInView } from "./logIn/login"; // ✅ Verifica la ruta del archivo
+export { default as LogInView } from "./logIn/login"; 
