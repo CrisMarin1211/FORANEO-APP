4b6d43e4152d0c2194bbd4bc940c5e69f27514ae
CrisMarin1211 <alca81411@gmail.com>
2025-04-01 20:38:39 -0500

    Feat: Added NameRecipe component to Congratulation page

--- Foraneo-App/src/mainPages/Recipes/Pages/Congratulation/Congratulation.jsx
+++ Foraneo-App/src/mainPages/Recipes/Pages/Congratulation/Congratulation.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+
 import TitleCongrats from '../../Components/3.CongratsPlan/TitleCongrats/TitleCongrats';
 import ImageEmotions from '../../Components/3.CongratsPlan/ImageEmotions/ImageEmotions';
 import UnlockedMessage from '../../Components/3.CongratsPlan/UnlockedMessage/UnlockedMessage';
 import MealPlanPhrase from '../../Components/3.CongratsPlan/MealPlanPhrase/MealPlanPhrase';
+import NameRecipe from '../../Components/3.CongratsPlan/NameRecipe/NameRecipe';
 
-function Congratulations() {
+function Congratulation() {
 	return (
 		<div>
 			<TitleCongrats />
 			<ImageEmotions />
 			<UnlockedMessage />
 			<MealPlanPhrase />
+			<NameRecipe />
 		</div>
 	);
 }
 
-export default Congratulations;
+export default Congratulation;
