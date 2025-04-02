c9af40cf4f79e38a113858561c51807196cd94c7
Azuki19 <analucia27259@gmail.com>
2025-04-02 18:00:34 -0500

    feat: export FinancesView (FinancesPage)

--- Foraneo-App/src/mainPages/export.jsx
+++ Foraneo-App/src/mainPages/export.jsx
@@ -1,3 +1,5 @@
-export { default as SignInView } from "./signIn/signin";
-export { default as LogInView } from "./logIn/login";
-export { default as MainView } from "./main/main";
+export { default as SignInView } from './signIn/signin';
+export { default as LogInView } from './logIn/login';
+export { default as MainView } from './main/main';
+export { default as FinancesView } from './finances/pages/finances/finances';
+
