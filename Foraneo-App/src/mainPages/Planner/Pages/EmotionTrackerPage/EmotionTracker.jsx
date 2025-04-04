b617143cf238825968b8a0968148aed54b769454
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-03 20:46:56 -0500

    Refactor: The structure of the ModalSaveEmotions component is adjusted and used in the EmotionTracker page

--- Foraneo-App/src/mainPages/Planner/Pages/EmotionTrackerPage/EmotionTracker.jsx
+++ Foraneo-App/src/mainPages/Planner/Pages/EmotionTrackerPage/EmotionTracker.jsx
@@ -3,12 +3,14 @@ import './EmotionTracker.css';
 import Menu from '../../components/navBar/navBar';
 import ListEmotions from '../../components/listEmotions/listEmotions';
 import { emotionsData } from '../../data/iconsData';
+import ModalSaveEmotions from '../../components/modalSaveEmotions/modalSaveEmotions';
 
 const EmotionTracker = () => {
 	return (
 		<>
 			<Menu />
 			<ListEmotions title='Emotions' emotions={emotionsData} />
+			<ModalSaveEmotions />
 		</>
 	);
 };
--- Foraneo-App/src/mainPages/Planner/components/modalSaveEmotions/modalSaveEmotions.jsx
+++ Foraneo-App/src/mainPages/Planner/components/modalSaveEmotions/modalSaveEmotions.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './modalSaveEmotions.css';
 
 const ModalSaveEmotions = () => {
+	const [isOpen, setIsOpen] = useState(true);
+	const handlerClose = () => {
+		setIsOpen(false);
+	};
+
+	if (!isOpen) return null;
+
 	return (
 		<>
 			<div className='modal-overlay'>
 				<div className='modal-content'>
-					<button className='close-button'>X</button>
+					<button className='close-button' onClick={handlerClose}>
+						X
+					</button>
 					<h2 className='modal-title'>Congratulations!</h2>
 					<p className='modal-description'>Congrats, you have registered your first day!</p>
 					<img src='/images/emotionsIcons.png' alt='emotions icons' className='modal-image'></img>
