565095c74f8fedda258ba081b3760abc58c96444
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-05 16:09:20 -0500

    Feat: Create the IconCircle component

---
+++ Foraneo-App/src/mainPages/Planner/components/iconCircle/iconCircle.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './IconCircle.css';
+
+const IconCircle2 = ({ icon }) => {
+	return <div className='icon-circle'>{icon}</div>;
+};
+
+export default IconCircle2;
