d6ca6b701c6729ffaef9d36ae96ba7aebdccfe3e
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-04 20:45:08 -0500

    Refactor: The structure of the ToDoList component is adjusted and the ModalAddTask is used in it

--- Foraneo-App/src/mainPages/Planner/components/toDoList/toDoList.jsx
+++ Foraneo-App/src/mainPages/Planner/components/toDoList/toDoList.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './toDoList.css';
+import ModalAddTask from '../modalAddTask/modalAddTask';
 
 const ToDoList = () => {
+	const [showModal, setShowModal] = useState(false);
+
+	const [tasks, setTasks] = useState(() => {
+		return JSON.parse(localStorage.getItem('tasks')) || [];
+	});
+
+	const handlerAddTask = (newTask) => {
+		if (!newTask.trim()) return;
+		const taskExist = tasks.some((t) => t.text.toLowerCase() === newTask.toLowerCase());
+		if (taskExist) return;
+		const updateTasks = [...tasks, { text: newTask, status: 'Pending' }];
+		setTasks(updateTasks);
+	};
+
+	useEffect(() => {
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}, [tasks]);
+
 	return (
 		<>
 			<section className='todo-container'>
 				<h2 className='todo-title'>TO-DO</h2>
 				<div className='task-list'>
-					<div className='task-item'>
-						<span className='task-text'>Do HCI homework</span>
-						<button className='task-status'>Not started</button>
-					</div>
+					{tasks.map((task, index) => (
+						<div className='task-item' key={index}>
+							<span className='task-text'>{task.text}</span>
+							<select
+								className={`task-status ${task.status.toLowerCase().replace(' ', '')}`}
+								value={task.status}
+								onChange={(e) => {
+									const updated = [...tasks];
+									updated[index].status = e.target.value;
+									setTasks(updated);
+								}}
+							>
+								<option value='Pending'>Pending</option>
+								<option value='In Progress'>In Progress</option>
+								<option value='Completed'>Completed</option>
+							</select>
+						</div>
+					))}
 				</div>
-				<button className='new-task-btn'>+ New Task</button>
+				<button className='new-task-btn' onClick={() => setShowModal(true)}>
+					+ New Task
+				</button>
 			</section>
+			{showModal && <ModalAddTask onClose={() => setShowModal(false)} onAdd={handlerAddTask} />}
 		</>
 	);
 };
