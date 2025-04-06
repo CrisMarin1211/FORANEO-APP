5bd4c0279b9673bcf395ce354ebfb76be240bdd0
CrisMarin1211 <alca81411@gmail.com>
2025-04-06 18:20:20 -0500

    feat: add WeeklyPlan page to App

--- Foraneo-App/src/App.jsx
+++ Foraneo-App/src/App.jsx
@@ -3,13 +3,15 @@ import './App.css';
 import Welcome from './mainPages/Recipes/Pages/WelcomeRecipes/Welcome';
 import PlanCreate from './mainPages/Recipes/Pages/PlanCreate/PlanCreate';
 import Congratulation from './mainPages/Recipes/Pages/Congratulation/Congratulation';
+import WeeklyPlan from './mainPages/Recipes/Pages/WeeklyPlan/WeeklyPlan';
 
 function App() {
 	return (
 		<div className='App'>
 			{/* <Welcome /> */}
 			{/* <PlanCreate /> */}
-			<Congratulation />
+			{/* <Congratulation /> */}
+			<WeeklyPlan />
 		</div>
 	);
 }
