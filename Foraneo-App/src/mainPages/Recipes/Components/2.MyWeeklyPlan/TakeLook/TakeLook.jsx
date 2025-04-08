46315edf2a56a015be36de092ef13feafaa8d810
CrisMarin1211 <alca81411@gmail.com>
2025-04-07 23:41:08 -0500

    feat: Add TakeLook title component

---
+++ Foraneo-App/src/mainPages/Recipes/Components/2.MyWeeklyPlan/TakeLook/TakeLook.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TakeLook.css';
+
+function TakeLook() {
+	return (
+		<section className='Take-Look'>
+			<p>Take a look at your plan</p>
+		</section>
+	);
+}
+
+export default TakeLook;
