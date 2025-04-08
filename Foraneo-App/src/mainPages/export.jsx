21a653f28f99309021d65a4fd923bd063613148b
CrisMarin1211 <alca81411@gmail.com>
2025-04-08 16:57:12 -0500

    feat: Add export for WelcomeView in export.jsx

--- Foraneo-App/src/mainPages/export.jsx
+++ Foraneo-App/src/mainPages/export.jsx
@@ -1,2 +1,3 @@
+export { default as WelcomeView } from './Recipes/Pages/WelcomeRecipes/Welcome';
 export { default as PlanCreateView } from './Recipes/Pages/PlanCreate/PlanCreate';
 export { default as WeeklyPlanView } from './Recipes/Pages/WeeklyPlan/WeeklyPlan';
