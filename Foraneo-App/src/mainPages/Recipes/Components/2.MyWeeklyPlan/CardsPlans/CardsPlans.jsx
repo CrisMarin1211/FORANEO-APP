4ff8f069042405fee189c69126c3a1da876f7b02
CrisMarin1211 <alca81411@gmail.com>
2025-04-08 15:49:32 -0500

    feat: Add sorting function to order weekly plans by day in CardsPlans component

--- Foraneo-App/src/mainPages/Recipes/Components/2.MyWeeklyPlan/CardsPlans/CardsPlans.jsx
+++ Foraneo-App/src/mainPages/Recipes/Components/2.MyWeeklyPlan/CardsPlans/CardsPlans.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import './CardsPlans.css';
 
 function CardsPlans({ plans }) {
+	const dayOrder = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+	const sortedPlans = [...plans].sort((a, b) => {
+		return dayOrder.indexOf(a.day) - dayOrder.indexOf(b.day);
+	});
+
 	return (
 		<section className='cards-wrapper'>
-			{plans.map((plan, index) => (
+			{sortedPlans.map((plan, index) => (
 				<section key={index} className='Cards-Plans'>
 					<img src={plan.breakfast.image} alt={plan.day} className='Card-image' />
 					<p className='Card-day'>{plan.day}</p>
