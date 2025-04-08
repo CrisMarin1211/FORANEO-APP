63dbeb129856360590efd499308d5c50c92fe5ac
CrisMarin1211 <alca81411@gmail.com>
2025-04-08 16:59:25 -0500

    feat: Add route for WelcomeView in router.jsx

--- Foraneo-App/src/router/router.jsx
+++ Foraneo-App/src/router/router.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
+import { WelcomeView } from '../mainPages/export';
 import { PlanCreateView } from '../mainPages/export';
 import { WeeklyPlanView } from '../mainPages/export';
 
 function Router() {
 	return (
 		<Routes>
-			<Route path='/' element={<PlanCreateView />} />
+			<Route path='/' element={<WelcomeView />} />
+			<Route path='/plancreate' element={<PlanCreateView />} />
 			<Route path='/weeklyplan' element={<WeeklyPlanView />} />
 		</Routes>
 	);
