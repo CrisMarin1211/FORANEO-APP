0f19c633d727f70938e9102b4a88f2ee10bd57ac
Azuki19 <analucia27259@gmail.com>
2025-04-08 19:28:53 -0500

    fix: ShowMoney component now shows actual user balance

--- Foraneo-App/src/mainPages/finances/components/showMoney/showMoney.jsx
+++ Foraneo-App/src/mainPages/finances/components/showMoney/showMoney.jsx
@@ -3,35 +3,45 @@ import { useNavigate } from 'react-router-dom';
 import { PlusOutlined, ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
 import './showMoney.css';
 
-const ShowMoney = () => {
+const ShowMoney = ({ totalExpenses, totalIncomes, balance }) => {
   const navigate = useNavigate();
-
-  // Datos quemados (ejemplo)
-  const income = 23000;
-  const expenses = 150000;
-  const balance = income - expenses;
 
   return (
     <section className="money-container">
-      <section className="money-card expense">
-        <h2><ArrowDownOutlined className="icon expense-icon" /> ${expenses.toLocaleString()}</h2>
-        <p>Expense</p>
-        <button className="add-btn" onClick={() => navigate('/add')}>
-          <PlusOutlined /> Add
-        </button>
+      <section className="top-section">
+        <section className="money-card expense">
+          <h2 className="h2text expenseValue">
+            <ArrowDownOutlined className="expense-icon" /> ${totalExpenses.toLocaleString()}
+          </h2>
+          <p>Expense</p>
+          <button
+            className="add-btn"
+            onClick={() => navigate('/add?tab=Expenses')}  // Enviar parámetro de tab
+          >
+            <PlusOutlined /> Add
+          </button>
+        </section>
+
+        <section className="money-card income">
+          <h2 className="h2text">
+            <ArrowUpOutlined className="income-icon" /> ${totalIncomes.toLocaleString()}
+          </h2>
+          <p>Income</p>
+          <button
+            className="add-btn"
+            onClick={() => navigate('/add?tab=Incomes')}  // Enviar parámetro de tab
+          >
+            <PlusOutlined /> Add
+          </button>
+        </section>
       </section>
 
-      <section className="money-card income">
-        <h2><ArrowUpOutlined className="icon income-icon" /> ${income.toLocaleString()}</h2>
-        <p>Income</p>
-        <button className="add-btn" onClick={() => navigate('/add')}>
-          <PlusOutlined /> Add
-        </button>
-      </section>
-
-      <section className="money-card balance">
-        <h2>${balance.toLocaleString()}</h2>
-        <p> ✅ Total balance</p>
+      <section className="balance-container">
+        <section className="money-card-balance">
+          <p className='ptotal' >✦ Total balance: </p>
+          <br></br>
+          <h2 className={`h2text1`}> ${balance.toLocaleString()}</h2>
+        </section>
       </section>
     </section>
   );
