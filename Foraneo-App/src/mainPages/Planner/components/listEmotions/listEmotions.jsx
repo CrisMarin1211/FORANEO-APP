7caccdff002c84b10165ea78ac9b746a2678f291
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-08 23:17:55 -0500

    feat: a div is created

--- Foraneo-App/src/mainPages/Planner/components/listEmotions/listEmotions.jsx
+++ Foraneo-App/src/mainPages/Planner/components/listEmotions/listEmotions.jsx
@@ -20,6 +20,7 @@ const ListEmotions = ({ title, emotions, onSelectionChange }) => {
 	return (
 		<>
 			<section className='emotion-container'>
+				<div></div>
 				<h2 className='emotion-title'>{title}</h2>
 				<div className='emotion-list'>
 					{emotions.map((emotion, index) => (
