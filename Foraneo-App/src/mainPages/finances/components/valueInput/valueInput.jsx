5f2fa07717124ddc7732d7a3da15ba5e87c4f116
Azuki19 <analucia27259@gmail.com>
2025-04-08 19:20:39 -0500

    feat: valueInput is created and structured to send the value of every expense or income

---
+++ Foraneo-App/src/mainPages/finances/components/valueInput/valueInput.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './valueInput.css';
+
+const ValueInput = ({ setValue, value }) => {
+  const increment = () => setValue(value + 1000);
+  const decrement = () => setValue(value - 1000);
+
+  return (
+    <section className='valueInputAll'>
+      <h2 className='valueInputTittle' ></h2>
+      <section className='valueInputGeneralContainer'>
+        <section className='valueInputContainer'>
+          <button className='valueInputButton' onClick={decrement}>-</button>
+
+          <section className='valueInputWrapper'>
+            <span className='valueInputCurrencySymbol'>$</span>
+            <input
+              type="number"
+              value={value}
+              onChange={(e) => setValue(parseInt(e.target.value))} // Actualiza el valor correctamente
+              className='valueInputInput'
+            />
+          </section>
+
+          <button className='valueInputButton' onClick={increment}>+</button>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default ValueInput;
