ef6f084feac52462165f00ac6199a5ccd9d01dc4
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-08 19:28:11 -0500

    feat: the year useState is eliminated

--- Foraneo-App/src/mainPages/Planner/components/calendar/calendar.jsx
+++ Foraneo-App/src/mainPages/Planner/components/calendar/calendar.jsx
@@ -5,7 +5,6 @@ import { faces } from '../../data/imagesData';
 const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const Calendar = () => {
-	const [year, setYear] = useState(2025);
 	const [month, setMonth] = useState(new Date().getMonth());
 	const [moodData, setMoodData] = useState({});
 
