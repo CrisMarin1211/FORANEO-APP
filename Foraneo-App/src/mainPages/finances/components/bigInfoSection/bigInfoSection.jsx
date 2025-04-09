cd5556ed04ed2d64d47820a00964201549845959
Azuki19 <analucia27259@gmail.com>
2025-04-08 19:16:52 -0500

    feat: BigInfoSection is modified to show the data sent

--- Foraneo-App/src/mainPages/finances/components/bigInfoSection/bigInfoSection.jsx
+++ Foraneo-App/src/mainPages/finances/components/bigInfoSection/bigInfoSection.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './bigInfoSection.css';
+import Timeline from '../timeline/timeline';
 
 const BigInfoSection = () => {
   const [activeTab, setActiveTab] = useState('Timeline');
+  const [savedData, setSavedData] = useState([]); // Aquí guardamos los datos de los tickets
 
+  // Cargar los datos de localStorage cuando el componente se monta
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('savedData')) || [];  // Si no hay datos, un array vacío
+    setSavedData(storedData); // Establecer los datos en el estado
+  }, []);
+
+  // Función para cambiar las pestañas
   const renderContent = () => {
     switch (activeTab) {
       case 'Timeline':
-        return <p>Data timeline lorem ipsum.</p>;
+        return <Timeline data={savedData} />;  // Solo renderizamos Timeline cuando la pestaña es 'Timeline'
       case 'Statistics':
         return <p>Data Statistics Lorem Ipsum.</p>;
       case 'Goal':
@@ -24,19 +33,19 @@ const BigInfoSection = () => {
           className={`tab ${activeTab === 'Timeline' ? 'active Timeline-tab' : ''}`}
           onClick={() => setActiveTab('Timeline')}
         >
-          Timeline
+          📃 Timeline
         </section>
         <section
           className={`tab ${activeTab === 'Statistics' ? 'active Statistics-tab' : ''}`}
           onClick={() => setActiveTab('Statistics')}
         >
-          Statistics
+          📊 Statistics
         </section>
         <section
           className={`tab ${activeTab === 'Goal' ? 'active Goal-tab' : ''}`}
           onClick={() => setActiveTab('Goal')}
         >
-          Goal
+          🎯 Goal
         </section>
       </section>
 
