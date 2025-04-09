702c333febd930a3c7b921ac76e99190de96407b
CrisMarin1211 <alca81411@gmail.com>
2025-04-09 04:05:29 -0500

    feat: Export UnlockedRecipesView for routing and use in the app

--- Foraneo-App/src/mainPages/export.jsx
+++ Foraneo-App/src/mainPages/export.jsx
@@ -2,3 +2,4 @@ export { default as WelcomeView } from './Recipes/Pages/WelcomeRecipes/Welcome';
 export { default as PlanCreateView } from './Recipes/Pages/PlanCreate/PlanCreate';
 export { default as WeeklyPlanView } from './Recipes/Pages/WeeklyPlan/WeeklyPlan';
 export { default as EditRecipeView } from './Recipes/Pages/EditRecipe/EditRecipe';
+export { default as UnlockedRecipesView } from './Recipes/Pages/UnlockedRecipes/UnlockedRecipes';
