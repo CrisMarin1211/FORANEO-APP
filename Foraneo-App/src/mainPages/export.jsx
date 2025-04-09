ffe569c8bdf761c823e38a216a3ff09eecf050dd
Azuki19 <analucia27259@gmail.com>
2025-04-08 19:13:40 -0500

    feat: "AddView" is exported and added in router

--- Foraneo-App/src/mainPages/export.jsx
+++ Foraneo-App/src/mainPages/export.jsx
@@ -2,4 +2,5 @@ export { default as SignInView } from './signIn/signin';
 export { default as LogInView } from './logIn/login';
 export { default as MainView } from './main/main';
 export { default as FinancesView } from './finances/pages/finances/finances';
+export { default as AddView } from './finances/pages/add/add';
 
--- Foraneo-App/src/router/router.jsx
+++ Foraneo-App/src/router/router.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { SignInView, LogInView, MainView, FinancesView } from '../mainPages/export';
+import { SignInView, LogInView, MainView, FinancesView, AddView } from '../mainPages/export';
 
 function Router() {
 	return (
@@ -8,6 +8,7 @@ function Router() {
 			<Route path='/login' element={<LogInView />} />
 			<Route path='/main' element={<MainView />} />
 			<Route path='/finances' element={<FinancesView />} />
+			<Route path='/add' element={<AddView />} />
 		</Routes>
 	);
 }
