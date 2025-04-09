b5ef5f182b0edf0435ae95016e119d20d8ee3eff
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-08 23:23:22 -0500

    feat: a new span is created

--- Foraneo-App/src/mainPages/Planner/components/listEmotions/listEmotions.jsx
+++ Foraneo-App/src/mainPages/Planner/components/listEmotions/listEmotions.jsx
@@ -22,6 +22,7 @@ const ListEmotions = ({ title, emotions, onSelectionChange }) => {
 			<section className='emotion-container'>
 				<div className='emotion-header' onClick={() => setIsOpen(!isOpen)}>
 					<h2 className='emotion-title'>{title}</h2>
+					<span></span>
 				</div>
 				<div className='emotion-list'>
 					{emotions.map((emotion, index) => (
