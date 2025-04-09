b4f61763c8c14eee7e384de845d1a54550a106d3
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-08 19:39:05 -0500

    refactor: const daysInMonth is eliminated

--- Foraneo-App/src/mainPages/Planner/components/calendar/calendar.jsx
+++ Foraneo-App/src/mainPages/Planner/components/calendar/calendar.jsx
@@ -33,7 +33,6 @@ const Calendar = () => {
 		setDayFeedbacks(storedFeedbacks);
 	}, []);
 
-	const daysInMonth = new Date(year, month + 1, 0).getDate();
 	const calendarDays = [];
 	for (let i = 0; i < firstDay; i++) {
 		calendarDays.push(null);
