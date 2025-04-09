39580b0c4f148c7f2168cf3af84abfe8134b8c82
CrisMarin1211 <alca81411@gmail.com>
2025-04-09 04:04:59 -0500

    feat: Add route for UnlockedRecipes page

--- Foraneo-App/src/router/router.jsx
+++ Foraneo-App/src/router/router.jsx
@@ -3,6 +3,7 @@ import { WelcomeView } from '../mainPages/export';
 import { PlanCreateView } from '../mainPages/export';
 import { WeeklyPlanView } from '../mainPages/export';
 import { EditRecipeView } from '../mainPages/export';
+import { UnlockedRecipesView } from '../mainPages/export';
 
 function Router() {
 	return (
@@ -11,6 +12,7 @@ function Router() {
 			<Route path='/plancreate' element={<PlanCreateView />} />
 			<Route path='/weeklyplan' element={<WeeklyPlanView />} />
 			<Route path='/editrecipe/:day' element={<EditRecipeView />} />
+			<Route path='/unlockedrecipes/:day/:mealTime' element={<UnlockedRecipesView />} />
 		</Routes>
 	);
 }
