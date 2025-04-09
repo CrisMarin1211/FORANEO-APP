d4fb9c0a1e2f1cf9c5ee513c75db0007c1d1eb27
CrisMarin1211 <alca81411@gmail.com>
2025-04-09 01:05:09 -0500

    feat: Add structure and functionality to Welcome page

--- Foraneo-App/src/mainPages/Recipes/Pages/WelcomeRecipes/Welcome.jsx
+++ Foraneo-App/src/mainPages/Recipes/Pages/WelcomeRecipes/Welcome.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import PlanYet from '../../Components/PlanYetPhrase/PlanYetPhrase';
 import Title from '../../Components/TitleWelcome/TitleWelcome';
 import ImageSad from '../../Components/imageSad/imageSad';
 import ButtonCreatePlan from '../../Components/ButtonCreatePlan/ButtonCreatePlan';
+import './Welcome.css';
 
 function Welcome() {
+	const navigate = useNavigate();
+
+	const handleCreatePlanClick = () => {
+		navigate('/plancreate');
+	};
+
 	return (
 		<div>
-			<Title />
+			<div className='tittle-welcome'>
+				<Title />
+			</div>
 			<ImageSad />
 			<PlanYet />
-			<ButtonCreatePlan />
+			<ButtonCreatePlan onClick={handleCreatePlanClick} />
 		</div>
 	);
 }
