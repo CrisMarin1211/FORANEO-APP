a82ba1cf1b844fc8ae164a1af7129f7f04b4619f
CrisMarin1211 <alca81411@gmail.com>
2025-04-09 00:53:05 -0500

    feat: Add navigation to edit recipe page from CardsPlans component

--- Foraneo-App/src/mainPages/Recipes/Components/2.MyWeeklyPlan/CardsPlans/CardsPlans.jsx
+++ Foraneo-App/src/mainPages/Recipes/Components/2.MyWeeklyPlan/CardsPlans/CardsPlans.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import './CardsPlans.css';
+import { useNavigate } from 'react-router-dom';
 
 function CardsPlans({ plans }) {
 	const dayOrder = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+	const navigate = useNavigate();
 
 	const sortedPlans = [...plans].sort((a, b) => {
 		return dayOrder.indexOf(a.day) - dayOrder.indexOf(b.day);
 	});
 
+	const handleCardClick = (day) => {
+		navigate(`/editrecipe/${day}`);
+	};
+
 	return (
 		<section className='cards-wrapper'>
 			{sortedPlans.map((plan, index) => (
-				<section key={index} className='Cards-Plans'>
+				<section key={index} className='Cards-Plans' onClick={() => handleCardClick(plan.day)}>
 					<img src={plan.breakfast.image} alt={plan.day} className='Card-image' />
 					<p className='Card-day'>{plan.day}</p>
 				</section>
