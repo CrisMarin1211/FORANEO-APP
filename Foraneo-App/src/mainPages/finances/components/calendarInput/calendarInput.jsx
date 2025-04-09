792a1dbbe448c50bdc53e18f94490e8e76edab2d
Azuki19 <analucia27259@gmail.com>
2025-04-08 23:53:13 -0500

    feat: calendarInput linked to css

--- Foraneo-App/src/mainPages/finances/components/calendarInput/calendarInput.jsx
+++ Foraneo-App/src/mainPages/finances/components/calendarInput/calendarInput.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DatePicker, Space } from 'antd';
 import moment from 'moment'; // Asegúrate de tener moment importado
+import './calendarInput.css'
 
 const CalendarInput = ({ setDate, date }) => {
   const onChange = (date, dateString) => {
@@ -11,6 +12,7 @@ const CalendarInput = ({ setDate, date }) => {
   return (
     <Space direction="vertical">
       <DatePicker
+        className='calendarInput'
         onChange={onChange}
         value={date ? moment(date, 'YYYY-MM-DD') : null} // Usamos `moment` para convertir la fecha
       />
