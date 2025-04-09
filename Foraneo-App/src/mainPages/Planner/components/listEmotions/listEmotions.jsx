e421e53ddc8d7d593613a150b35cb9ddc3d99cd9
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-08 23:18:48 -0500

    feat: a class name is given to the div

--- Foraneo-App/src/mainPages/Planner/components/listEmotions/listEmotions.jsx
+++ Foraneo-App/src/mainPages/Planner/components/listEmotions/listEmotions.jsx
@@ -20,7 +20,7 @@ const ListEmotions = ({ title, emotions, onSelectionChange }) => {
 	return (
 		<>
 			<section className='emotion-container'>
-				<div></div>
+				<div className='emotion-header'></div>
 				<h2 className='emotion-title'>{title}</h2>
 				<div className='emotion-list'>
 					{emotions.map((emotion, index) => (
