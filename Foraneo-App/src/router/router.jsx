339fc8143edf5e1e7c88db8eb1922d562573a156
CrisMarin1211 <alca81411@gmail.com>
2025-04-09 01:05:34 -0500

    feat: Add route for EditRecipeView in router

--- Foraneo-App/src/router/router.jsx
+++ Foraneo-App/src/router/router.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { WelcomeView } from '../mainPages/export';
 import { PlanCreateView } from '../mainPages/export';
 import { WeeklyPlanView } from '../mainPages/export';
+import { EditRecipeView } from '../mainPages/export';
 
 function Router() {
 	return (
@@ -9,6 +10,7 @@ function Router() {
 			<Route path='/' element={<WelcomeView />} />
 			<Route path='/plancreate' element={<PlanCreateView />} />
 			<Route path='/weeklyplan' element={<WeeklyPlanView />} />
+			<Route path='/editrecipe/:day' element={<EditRecipeView />} />
 		</Routes>
 	);
 }
