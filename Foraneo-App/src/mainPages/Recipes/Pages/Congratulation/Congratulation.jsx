76d366b1573cc2c0160b20817a6a3989016757b6
CrisMarin1211 <alca81411@gmail.com>
2025-04-10 00:55:38 -0500

    Fix: remove unnecessary space in Congratulation.jsx

--- Foraneo-App/src/mainPages/Recipes/Pages/Congratulation/Congratulation.jsx
+++ Foraneo-App/src/mainPages/Recipes/Pages/Congratulation/Congratulation.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import TitleCongrats from '../../Components/3.CongratsPlan/TitleCongrats/TitleCongrats';
 import ImageEmotions from '../../Components/3.CongratsPlan/ImageEmotions/ImageEmotions';
 import UnlockedMessage from '../../Components/3.CongratsPlan/UnlockedMessage/UnlockedMessage';
