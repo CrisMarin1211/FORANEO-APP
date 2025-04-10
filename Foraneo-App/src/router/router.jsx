a5f5541592ded182883dacb5b8164fb9381c1ed1
Azuki19 <analucia27259@gmail.com>
2025-04-10 00:23:37 -0500

    feat: added planner routs to router

--- Foraneo-App/src/router/router.jsx
+++ Foraneo-App/src/router/router.jsx
@@ -1,5 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import { SignInView, LogInView, MainView, FinancesView, AddView } from '../mainPages/export';
+import {
+	SignInView,
+	LogInView,
+	MainView,
+	FinancesView,
+	AddView,
+	EmotionTrackerView,
+	CalendarPageView,
+	ToDoPageView,
+} from '../mainPages/export';
 
 function Router() {
 	return (
@@ -9,6 +18,9 @@ function Router() {
 			<Route path='/main' element={<MainView />} />
 			<Route path='/finances' element={<FinancesView />} />
 			<Route path='/add' element={<AddView />} />
+			<Route path='/emotions' element={<EmotionTrackerView />} />
+			<Route path='/calendar' element={<CalendarPageView />} />
+			<Route path='/to-do' element={<ToDoPageView />} />
 		</Routes>
 	);
 }
