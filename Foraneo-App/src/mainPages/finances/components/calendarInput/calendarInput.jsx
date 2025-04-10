74b48050460a716330d7b7c252d8cdfa3267914a
Azuki19 <analucia27259@gmail.com>
2025-04-10 00:32:21 -0500

    fix: cleaned comments in calendarInput

--- Foraneo-App/src/mainPages/finances/components/calendarInput/calendarInput.jsx
+++ Foraneo-App/src/mainPages/finances/components/calendarInput/calendarInput.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { DatePicker, Space } from 'antd';
-import moment from 'moment'; // Asegúrate de tener moment importado
+import moment from 'moment'; 
 import './calendarInput.css'
 
 const CalendarInput = ({ setDate, date }) => {
   const onChange = (date, dateString) => {
-    console.log('Fecha seleccionada:', dateString); // Verifica el valor de la fecha seleccionada
-    setDate(dateString); // Actualizamos el estado con la fecha seleccionada
+
+    setDate(dateString);
   };
 
   return (
@@ -14,7 +14,7 @@ const CalendarInput = ({ setDate, date }) => {
       <DatePicker
         className='calendarInput'
         onChange={onChange}
-        value={date ? moment(date, 'YYYY-MM-DD') : null} // Usamos `moment` para convertir la fecha
+        value={date ? moment(date, 'YYYY-MM-DD') : null}
       />
     </Space>
   );
