2b3ed19c3aad5c6d22a5176a445a1c0268186ed8
Azuki19 <analucia27259@gmail.com>
2025-04-10 00:34:21 -0500

    fix: deleted an import router

--- Foraneo-App/src/App.jsx
+++ Foraneo-App/src/App.jsx
@@ -1,8 +1,6 @@
-
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Router from './router/router';
-import Router from './routerPlanner/router';
 
 function App() {
 	return (
