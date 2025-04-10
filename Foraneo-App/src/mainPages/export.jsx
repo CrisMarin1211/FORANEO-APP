f0e143494d454ddac216849bf12e023d229c6a1b
Azuki19 <analucia27259@gmail.com>
2025-04-10 00:33:42 -0500

    feat: exported planner respective routes

--- Foraneo-App/src/mainPages/export.jsx
+++ Foraneo-App/src/mainPages/export.jsx
@@ -3,4 +3,6 @@ export { default as LogInView } from './logIn/login';
 export { default as MainView } from './main/main';
 export { default as FinancesView } from './finances/pages/finances/finances';
 export { default as AddView } from './finances/pages/add/add';
-
+export {default as CalendarPageView} from './Planner/Pages/CalendarPage/CalendarPage'
+export {default as ToDoPageView} from './Planner/Pages/ToDoPage/ToDoPage'
+export {default as EmotionTrackerView} from './Planner/Pages/EmotionTrackerPage/EmotionTracker'
