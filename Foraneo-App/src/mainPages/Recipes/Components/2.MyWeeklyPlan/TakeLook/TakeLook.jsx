aa379d047efcedaf53ccf5db61901db753bbd12b
CrisMarin1211 <alca81411@gmail.com>
2025-04-10 03:21:12 -0500

    refactor: remove class from section and add it to p in TakeLook component

--- Foraneo-App/src/mainPages/Recipes/Components/2.MyWeeklyPlan/TakeLook/TakeLook.jsx
+++ Foraneo-App/src/mainPages/Recipes/Components/2.MyWeeklyPlan/TakeLook/TakeLook.jsx
@@ -3,8 +3,8 @@ import './TakeLook.css';
 
 function TakeLook() {
 	return (
-		<section className='Take-Look'>
-			<p>Take a look at your plan</p>
+		<section>
+			<p className='Take-Look'>Take a look at your plan</p>
 		</section>
 	);
 }
