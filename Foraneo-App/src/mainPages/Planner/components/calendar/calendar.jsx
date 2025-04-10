2028ebc43cea42e56cfbbba734bc235e20240fe6
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-09 21:35:24 -0500

    refactor: the properties of the svg were adjusted

--- Foraneo-App/src/mainPages/Planner/components/calendar/calendar.jsx
+++ Foraneo-App/src/mainPages/Planner/components/calendar/calendar.jsx
@@ -103,7 +103,7 @@ const Calendar = () => {
 									<div className='calendar-item'>
 										<svg className='calendar-svg' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
 											<circle cx='50' cy='50' r='45' stroke='#79CFD9' strokeWidth='5' fill='white' />
-											{faceSrc && <image href={faceSrc} x='18' y='18' width='64' height='64' />}
+											{faceSrc && <image href={faceSrc} x='12' y='12' width='80' height='80' />}
 										</svg>
 										<div className='calendar-day-number'>{day}</div>
 									</div>
