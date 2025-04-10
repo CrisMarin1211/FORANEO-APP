591cd2d5b954eef22cdb0698c14392c93ee5dcbf
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-09 21:08:12 -0500

    fix: the error is fixed

--- Foraneo-App/src/mainPages/Planner/Pages/EmotionTrackerPage/EmotionTracker.jsx
+++ Foraneo-App/src/mainPages/Planner/Pages/EmotionTrackerPage/EmotionTracker.jsx
@@ -28,34 +28,33 @@ const EmotionTracker = () => {
 	};
 
 	const handlerSaveAll = () => {
-    if (!dayMood) {
-      alert("It is mandatory to select an emotion");
-      return;
-    }
+		if (!dayMood) {
+			alert('It is mandatory to select an emotion');
+			return;
+		}
 
-    Object.entries(allSelections).forEach(([title, items]) => {
-      const key = emotions_${title};
-      if (items.length > 0) {
-        localStorage.setItem(key, JSON.stringify(items));
-      } else {
-        localStorage.removeItem(key);
-      }
-    });
+		Object.entries(allSelections).forEach(([title, items]) => {
+			const key = `emotions_${title}`;
+			if (items.length > 0) {
+				localStorage.setItem(key, JSON.stringify(items));
+			} else {
+				localStorage.removeItem(key);
+			}
+		});
 
-    const existingFeedbacks =
-      JSON.parse(localStorage.getItem("day-feedbacks")) || {};
+		const existingFeedbacks = JSON.parse(localStorage.getItem('day-feedbacks')) || {};
 
-    const isFirstTime = !existingFeedbacks[selectedDate];
+		const isFirstTime = !existingFeedbacks[selectedDate];
 
-    existingFeedbacks[selectedDate] = dayMood;
-    localStorage.setItem("day-feedbacks", JSON.stringify(existingFeedbacks));
+		existingFeedbacks[selectedDate] = dayMood;
+		localStorage.setItem('day-feedbacks', JSON.stringify(existingFeedbacks));
 
-    if (isFirstTime) {
-      setShowModal(true);
-    } else {
-      alert("Emotion updated successfully for this day ✅");
-    }
-  };
+		if (isFirstTime) {
+			setShowModal(true);
+		} else {
+			alert('Emotion updated successfully for this day ✅');
+		}
+	};
 
 	const goToCalendar = () => {
 		navigate('/calendar');
