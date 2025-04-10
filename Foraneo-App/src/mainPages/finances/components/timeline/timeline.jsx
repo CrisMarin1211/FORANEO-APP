abfe2a66542daf40143d42a06e91b1092ddbeb72
Azuki19 <analucia27259@gmail.com>
2025-04-10 04:20:23 -0500

    feat: timeline adjustments to manage ticketModal

--- Foraneo-App/src/mainPages/finances/components/timeline/timeline.jsx
+++ Foraneo-App/src/mainPages/finances/components/timeline/timeline.jsx
@@ -1,70 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Day from './day/day';
 import Ticket from './ticket/ticket';
+import TicketModal from '../ticketModal/ticketModal';
 
 const Timeline = ({ data, selectedMonth }) => {
+  const [selectedTicket, setSelectedTicket] = useState(null);
 
   const filterTicketsByMonth = (data, month) => {
     if (!month) return data;
-
     const [selectedYear, selectedMonthIndex] = month.split('-');
-
     return data.filter(ticket => {
       const itemDate = new Date(ticket.date);
-      const itemYear = itemDate.getFullYear();
-      const itemMonth = itemDate.getMonth();
-
-      return itemYear === parseInt(selectedYear) && itemMonth === parseInt(selectedMonthIndex) - 1;
+      return itemDate.getFullYear() === parseInt(selectedYear) &&
+             itemDate.getMonth() === parseInt(selectedMonthIndex) - 1;
     });
   };
 
-
   const filteredTickets = filterTicketsByMonth(data, selectedMonth);
-
 
   const groupByDate = (data) => {
     return data.reduce((acc, ticket) => {
       const ticketDate = ticket.date;
-      if (!acc[ticketDate]) {
-        acc[ticketDate] = [];
-      }
+      if (!acc[ticketDate]) acc[ticketDate] = [];
       acc[ticketDate].push(ticket);
       return acc;
     }, {});
   };
 
-
   const groupedTickets = groupByDate(filteredTickets);
-
-
   const sortedDates = Object.keys(groupedTickets).sort((a, b) => new Date(b) - new Date(a));
 
   return (
-    <section >
-
+    <section>
       {sortedDates.map((date, index) => {
-
         const adjustedDate = new Date(date);
         adjustedDate.setDate(adjustedDate.getDate() + 1);
-
         return (
           <section key={index}>
             <Day date={adjustedDate.toISOString().split('T')[0]} />
-
-
             {groupedTickets[date].map((ticket, ticketIndex) => (
-              <Ticket
-                key={ticketIndex}
-                category={ticket.category}
-                value={ticket.value}
-                name={ticket.name}
-                details={ticket.details}
-                type={ticket.type}
-              />
+              <div key={ticketIndex} onClick={() => setSelectedTicket(ticket)}>
+                <Ticket
+                  category={ticket.category}
+                  value={ticket.value}
+                  name={ticket.name}
+                  details={ticket.details}
+                  type={ticket.type}
+                />
+              </div>
             ))}
           </section>
         );
       })}
+      <TicketModal ticket={selectedTicket} onClose={() => setSelectedTicket(null)} />
     </section>
   );
 };
