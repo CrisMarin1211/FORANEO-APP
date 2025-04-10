6c9782c2417f6019f5021c6fe93c0d01a3de7e3f
Azuki19 <analucia27259@gmail.com>
2025-04-10 00:30:30 -0500

    fix: cleaned comments in day component

--- Foraneo-App/src/mainPages/finances/components/timeline/day/day.jsx
+++ Foraneo-App/src/mainPages/finances/components/timeline/day/day.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import './day.css';
 
 const Day = ({ date }) => {
-  // Convertimos la fecha en un objeto Date
+
   const formattedDate = new Date(date);
 
-  // Extraemos el día, mes y día de la semana
+
   const day = formattedDate.getDate();
-  const month = formattedDate.toLocaleString('default', { month: 'long' }); // Mes
-  const weekday = formattedDate.toLocaleString('default', { weekday: 'short' }); // Día de la semana
+  const month = formattedDate.toLocaleString('default', { month: 'long' });
+  const weekday = formattedDate.toLocaleString('default', { weekday: 'short' });
 
   return (
     <section className="timeLineDate">
@@ -21,11 +21,7 @@ const Day = ({ date }) => {
         </h4>
       </section>
 
-      {/* Opcionalmente, puedes mostrar los valores de ingresos y gastos del día */}
-      {/* <section className="timeLineMiniValues">
-        <p className="incomesP">+ $12.000</p>
-        <p className="expensesP">-$6.000</p>
-      </section> */}
+
     </section>
   );
 };
