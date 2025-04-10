b2c3863083d8263c91c672df5e88c9e44a0d3b5a
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-09 20:44:31 -0500

    refactor: the structure of the navigation routes is adjusted

--- Foraneo-App/src/App.jsx
+++ Foraneo-App/src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import CalendarPage from './mainPages/Planner/Pages/CalendarPage/CalendarPage';
-import ToDoPage from './mainPages/Planner/Pages/ToDoPage/ToDoPage';
-import EmotionTracker from './mainPages/Planner/Pages/EmotionTrackerPage/EmotionTracker';
+import Router from './routerPlanner/router';
 
-const App = () => (
-	<BrowserRouter>
-		<Routes>
-			<Route path='/' element={<EmotionTracker />} />
-			<Route path='/calendar' element={<CalendarPage />} />
-			<Route path='/to-do' element={<ToDoPage />} />
-		</Routes>
-	</BrowserRouter>
-);
+function App() {
+	return (
+		<BrowserRouter>
+			<section className='App'>
+				<Router />
+			</section>
+		</BrowserRouter>
+	);
+}
 
 export default App;
---
+++ Foraneo-App/src/routerPlanner/router.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import CalendarPage from './mainPages/Planner/Pages/CalendarPage/CalendarPage';
+import ToDoPage from './mainPages/Planner/Pages/ToDoPage/ToDoPage';
+import EmotionTracker from './mainPages/Planner/Pages/EmotionTrackerPage/EmotionTracker';
+
+function Router() {
+	return (
+		<Routes>
+			<Route path='/' element={<EmotionTracker />} />
+			<Route path='/calendar' element={<CalendarPage />} />
+			<Route path='/to-do' element={<ToDoPage />} />
+		</Routes>
+	);
+}
+
+export default Router;
