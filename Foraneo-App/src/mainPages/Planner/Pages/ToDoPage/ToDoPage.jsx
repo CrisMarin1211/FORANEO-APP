069d50598c0c89d93b04becd7c3ff0b0a7b7f60c
anasofia01 <sofiapacheco0423@gmail.com>
2025-04-09 20:33:18 -0500

    feat: import of the useNavigate from react router dom

--- Foraneo-App/src/mainPages/Planner/Pages/ToDoPage/ToDoPage.jsx
+++ Foraneo-App/src/mainPages/Planner/Pages/ToDoPage/ToDoPage.jsx
@@ -5,6 +5,7 @@ import ToDoList from '../../components/toDoList/toDoList';
 import SummaryEmotions from '../../components/summaryEmotions/summaryEmotions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const ToDoPage = () => {
 	const navigate = useNavigate();
