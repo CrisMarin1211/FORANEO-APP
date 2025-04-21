64609042e501bc7aa80419e35d8308e70c5f3e81
Azuki19 <analucia27259@gmail.com>
2025-04-21 17:50:28 -0500

    feat: installing and setting redux

--- Foraneo-App/src/App.jsx
+++ Foraneo-App/src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Router from './router/router';
+import {Provider} from 'react-redux'
+import {store} from './redux/store'
+
 
 function App() {
 	return (
+		<Provider store={store}>
 		<BrowserRouter>
 			<section className='App'>
 				<Router />
 			</section>
 		</BrowserRouter>
+		</Provider>
 	);
 }
 
