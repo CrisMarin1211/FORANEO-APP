27ef9b8430066799c176d9a7df3d9842bd865ca3
Azuki19 <analucia27259@gmail.com>
2025-05-12 15:45:00 -0500

    feat: store is created

--- Foraneo-App/src/redux/store.js
+++ Foraneo-App/src/redux/store.js
@@ -1,7 +1,19 @@
-import {configureStore} from '@reduxjs/toolkit'
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import registerReducer from './signIn/registerSlice';  // Reducer para el registro de usuario
+import goalReducer from './finances/goalSlice/goalSlice'; // Reducer para la gestión de la meta de ahorro
+import financeReducer from './finances/financesSlice';  // Reducer para manejar las finanzas
 
+// Configuración del store de Redux
 export const store = configureStore({
+  reducer: {
+    register: registerReducer,
+    goal: goalReducer,
+    finances: financeReducer,  // Correcto: finanzas debe usar el reducer de financeSlice
+  },
+});
 
-
-  
-})
+// Exportar las acciones de los slices
+export { setUser, logoutUser } from './signIn/registerSlice';
+export { setGoal, clearGoal } from './finances/goalSlice/goalSlice';
+export { setTransactions, addTransaction, removeTransaction } from './finances/financesSlice';  // Exporta también las acciones de financeSlice
