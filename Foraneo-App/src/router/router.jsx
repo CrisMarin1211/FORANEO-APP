255b0e70f49ec762031b22e4b93755b7c401bfa0
Azuki19 <analucia27259@gmail.com>
2025-05-12 15:42:49 -0500

    feat: organized the importation of routes in reouter

--- Foraneo-App/src/router/router.jsx
+++ Foraneo-App/src/router/router.jsx
@@ -1,43 +1,84 @@
 import { Routes, Route } from 'react-router-dom';
-import { WelcomeView } from '../mainPages/export';
-import { PlanCreateView } from '../mainPages/export';
-import { WeeklyPlanView } from '../mainPages/export';
-import { EditRecipeView } from '../mainPages/export';
-import { UnlockedRecipesView } from '../mainPages/export';
-import { RecipeDetailView } from '../mainPages/export';
-import { CongratulationView } from '../mainPages/export';
-
-import {
-	SignInView,
-	LogInView,
-	MainView,
-	FinancesView,
-	AddView,
-	EmotionTrackerView,
-	CalendarPageView,
-	ToDoPageView,
-} from '../mainPages/export';
+import { useSelector } from 'react-redux'; // Accedemos al estado global de Redux
+import { WelcomeView, PlanCreateView, WeeklyPlanView, EditRecipeView, UnlockedRecipesView, RecipeDetailView, CongratulationView } from '../mainPages/export';
+import { SignInView, LogInView, MainView, FinancesView, AddView, EmotionTrackerView, CalendarPageView, ToDoPageView } from '../mainPages/export';
+import ProtectedRoute from '../mainPages/ProtectedRoute.jsX';
 
 function Router() {
-	return (
-		<Routes>
-			<Route path='/plancreate' element={<PlanCreateView />} />
-			<Route path='/welcome' element={<WelcomeView />} />
-			<Route path='/weeklyplan' element={<WeeklyPlanView />} />
-			<Route path='/editrecipe/:day' element={<EditRecipeView />} />
-			<Route path='/unlockedrecipes/:day/:mealTime' element={<UnlockedRecipesView />} />
-			<Route path='/recipedetail/:day/:mealTime/:recipeName' element={<RecipeDetailView />} />
-			<Route path='/congratulation' element={<CongratulationView />} />
-			<Route path='/' element={<SignInView />} />
-			<Route path='/login' element={<LogInView />} />
-			<Route path='/main' element={<MainView />} />
-			<Route path='/finances' element={<FinancesView />} />
-			<Route path='/add' element={<AddView />} />
-			<Route path='/emotions' element={<EmotionTrackerView />} />
-			<Route path='/calendar' element={<CalendarPageView />} />
-			<Route path='/to-do' element={<ToDoPageView />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      {/* Rutas públicas */}
+      <Route path="/" element={<SignInView />} />
+      <Route path="/login" element={<LogInView />} />
+
+      {/* Rutas protegidas */}
+      <Route path="/main" element={
+        <ProtectedRoute>
+          <MainView />
+        </ProtectedRoute>
+      } />
+      <Route path="/welcome" element={
+        <ProtectedRoute>
+          <WelcomeView />
+        </ProtectedRoute>
+      } />
+      <Route path="/plancreate" element={
+        <ProtectedRoute>
+          <PlanCreateView />
+        </ProtectedRoute>
+      } />
+      <Route path="/weeklyplan" element={
+        <ProtectedRoute>
+          <WeeklyPlanView />
+        </ProtectedRoute>
+      } />
+      <Route path="/editrecipe/:day" element={
+        <ProtectedRoute>
+          <EditRecipeView />
+        </ProtectedRoute>
+      } />
+      <Route path="/unlockedrecipes/:day/:mealTime" element={
+        <ProtectedRoute>
+          <UnlockedRecipesView />
+        </ProtectedRoute>
+      } />
+      <Route path="/recipedetail/:day/:mealTime/:recipeName" element={
+        <ProtectedRoute>
+          <RecipeDetailView />
+        </ProtectedRoute>
+      } />
+      <Route path="/congratulation" element={
+        <ProtectedRoute>
+          <CongratulationView />
+        </ProtectedRoute>
+      } />
+      <Route path="/finances" element={
+        <ProtectedRoute>
+          <FinancesView />
+        </ProtectedRoute>
+      } />
+      <Route path="/add" element={
+        <ProtectedRoute>
+          <AddView />
+        </ProtectedRoute>
+      } />
+      <Route path="/emotions" element={
+        <ProtectedRoute>
+          <EmotionTrackerView />
+        </ProtectedRoute>
+      } />
+      <Route path="/calendar" element={
+        <ProtectedRoute>
+          <CalendarPageView />
+        </ProtectedRoute>
+      } />
+      <Route path="/to-do" element={
+        <ProtectedRoute>
+          <ToDoPageView />
+        </ProtectedRoute>
+      } />
+    </Routes>
+  );
 }
 
 export default Router;
