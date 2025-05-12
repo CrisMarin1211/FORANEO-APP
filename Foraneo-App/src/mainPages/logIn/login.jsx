f75ae5bbb1465148236697a266045712a9e50c7d
Azuki19 <analucia27259@gmail.com>
2025-05-12 15:49:23 -0500

    feat: login auth

--- Foraneo-App/src/mainPages/logIn/login.jsx
+++ Foraneo-App/src/mainPages/logIn/login.jsx
@@ -2,29 +2,26 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import LogInForm from './components/logInForm/logInForm';
+import { login } from '../../utils/auth'; // Importa la función login para marcar la autenticación
 import './login.css';
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const onFinish = values => {
-    console.log('Success:', values);
-    if (values.remember) {
-      localStorage.setItem('username', values.username);
-      localStorage.setItem('password', values.password);
-    } else {
-      localStorage.removeItem('username');
-      localStorage.removeItem('password');
-    }
-    navigate('/finances'); 
+  const onFinish = (user) => {
+    // Si el login es exitoso, marcamos al usuario como autenticado
+    login();
+
+    // Navegamos a la página principal después del login
+    navigate('/main');
   };
 
-  const onFinishFailed = errorInfo => {
+  const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
 
   return (
-    <section className='login'>
+    <section className="login">
       <h1>Login</h1>
       <LogInForm onFinish={onFinish} onFinishFailed={onFinishFailed} />
       <p style={{ textAlign: 'center' }}>
