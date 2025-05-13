2afd2c58140d6e4fd90ac8bfe701729c27300697
Azuki19 <analucia27259@gmail.com>
2025-05-13 13:54:22 -0500

    feat: congratulation page now manage information

--- Foraneo-App/src/mainPages/Recipes/Pages/Congratulation/Congratulation.jsx
+++ Foraneo-App/src/mainPages/Recipes/Pages/Congratulation/Congratulation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TitleCongrats from '../../Components/3.CongratsPlan/TitleCongrats/TitleCongrats';
 import ImageEmotions from '../../Components/3.CongratsPlan/ImageEmotions/ImageEmotions';
 import UnlockedMessage from '../../Components/3.CongratsPlan/UnlockedMessage/UnlockedMessage';
@@ -6,15 +6,29 @@ import MealPlanPhrase from '../../Components/3.CongratsPlan/MealPlanPhrase/MealP
 import NameRecipe from '../../Components/3.CongratsPlan/NameRecipe/NameRecipe';
 
 function Congratulation() {
-	return (
-		<section>
-			<TitleCongrats />
-			<ImageEmotions />
-			<UnlockedMessage />
-			<MealPlanPhrase />
-			<NameRecipe />
-		</section>
-	);
+  const [completedRecipe, setCompletedRecipe] = useState(null);
+
+  useEffect(() => {
+    // Recuperar los datos de la receta completada desde localStorage
+    const savedRecipe = JSON.parse(localStorage.getItem('completedRecipe'));
+    if (savedRecipe) {
+      setCompletedRecipe(savedRecipe);
+    }
+  }, []);
+
+  if (!completedRecipe) return <div>Loading...</div>; // Mientras se carga la receta
+
+  return (
+    <section>
+      <TitleCongrats />
+      <ImageEmotions />
+      {/* Mostrar el mensaje de receta desbloqueada */}
+      <UnlockedMessage recipeName={completedRecipe.name} />
+      <MealPlanPhrase />
+      {/* Mostrar el nombre de la receta y la imagen */}
+      <NameRecipe recipeName={completedRecipe.name} recipeImage={completedRecipe.image} />
+    </section>
+  );
 }
 
 export default Congratulation;
