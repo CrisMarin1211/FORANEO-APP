0336f9a0d572b40b853c57b40c8ad1644987ac8d
Azuki19 <analucia27259@gmail.com>
2025-05-12 23:39:03 -0500

    feat:added Link to recipedeatil into CardsRecipe

--- Foraneo-App/src/mainPages/Recipes/Components/CardsRecipe/CardsRecipe.jsx
+++ Foraneo-App/src/mainPages/Recipes/Components/CardsRecipe/CardsRecipe.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './CardsRecipe.css';
+import { Link } from 'react-router-dom';  
 import ButtonLetsCook from '../ButtonLetsCook/ButtonLetsCook';
 
-function CardsRecipe({ mealTime, recipe }) {
+function CardsRecipe({ mealTime, recipe, day }) {
 	return (
 		<section className='recipe-card'>
 			<section className='recipe-header'>
@@ -16,7 +17,9 @@ function CardsRecipe({ mealTime, recipe }) {
 			<h4 className='recipe-name'>{recipe.name}</h4>
 			<p className='recipe-description'>{recipe.description}</p>
 
-			<ButtonLetsCook />
+			<Link to={`/recipedetail/${day}/${mealTime}/${recipe.name}`}>
+				<ButtonLetsCook />
+			</Link>
 		</section>
 	);
 }
