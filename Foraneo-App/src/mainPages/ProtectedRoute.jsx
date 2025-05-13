a1c7d78b5f278c6cb63106dda078efa3c46025c2
Azuki19 <analucia27259@gmail.com>
2025-05-12 23:40:22 -0500

    feat: protectedroute clean

--- Foraneo-App/src/mainPages/ProtectedRoute.jsx
+++ Foraneo-App/src/mainPages/ProtectedRoute.jsx
@@ -1,17 +1,13 @@
-import React from 'react'; // React es necesario para JSX
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-// Este componente protegerá las rutas que requieren autenticación
-const ProtectedRoute = ({ children }) => {
-  const user = useSelector((state) => state.register.user); // Accedemos al estado del usuario desde Redux
 
+const ProtectedRoute = ({ children }) => {
+  const user = useSelector((state) => state.register.user);
   if (!user) {
-    // Si no hay un usuario, redirigimos a login
     return <Navigate to="/login" />;
   }
-
-  // Si está autenticado, renderizamos los children (el componente recibido)
   return children;
 };
 
