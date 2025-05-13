30b34660bcade1e3c9dc1ddb598f57e4babc2e1c
Azuki19 <analucia27259@gmail.com>
2025-05-13 13:54:48 -0500

    feat: components from congrats page now send information

--- Foraneo-App/src/mainPages/Recipes/Components/3.CongratsPlan/NameRecipe/NameRecipe.jsx
+++ Foraneo-App/src/mainPages/Recipes/Components/3.CongratsPlan/NameRecipe/NameRecipe.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './NameRecipe.css';
 
-function NameRecipe() {
-	return (
-		<section className='Name-Recipe'>
-			<h2 className='title-NameRecipe'>Banana Oatmeal Bowl</h2>
-		</section>
-	);
+function NameRecipe({ recipeName, recipeImage }) {
+  return (
+    <section className='Name-Recipe'>
+      <h2 className='title-NameRecipe'>{recipeName}</h2>
+      <img className="recipe-image" src={recipeImage} alt={recipeName} />
+    </section>
+  );
 }
 
 export default NameRecipe;
