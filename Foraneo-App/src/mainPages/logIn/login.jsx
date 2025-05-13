334259f68813444997573b6b4d22c684e839080c
Azuki19 <analucia27259@gmail.com>
2025-05-12 23:41:57 -0500

    feat: cleaned login

--- Foraneo-App/src/mainPages/logIn/login.jsx
+++ Foraneo-App/src/mainPages/logIn/login.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import LogInForm from './components/logInForm/logInForm';
-import { login } from '../../utils/auth'; // Importa la función login para marcar la autenticación
+import { login } from '../../utils/auth'; 
 import './login.css';
 
 const Login = () => {
   const navigate = useNavigate();
 
   const onFinish = (user) => {
-    // Si el login es exitoso, marcamos al usuario como autenticado
     login();
 
-    // Navegamos a la página principal después del login
     navigate('/main');
   };
 
