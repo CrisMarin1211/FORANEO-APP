f9f6fba719c85c551592f9c95dbc162e9173ecea
Azuki19 <analucia27259@gmail.com>
2025-05-13 14:19:15 -0500

    feat: cards unlocked

--- Foraneo-App/src/mainPages/Recipes/Components/5.UnlockedFoods/CardsUnlocked/CardsUnlocked.jsx
+++ Foraneo-App/src/mainPages/Recipes/Components/5.UnlockedFoods/CardsUnlocked/CardsUnlocked.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import './CardsUnlocked.css';
 
 function CardsUnlocked({ recipe, onAddRecipe }) {
-	return (
-		<section className='unlocked-card'>
-			<img src={recipe.image} alt={recipe.name} className='unlocked-image' />
-			<h3>{recipe.name}</h3>
-			<ul className='ingredients-list'>
-				{recipe.ingredients.map((ingredient, index) => (
-					<li key={index}>{ingredient}</li>
-				))}
-			</ul>
-			<button onClick={() => onAddRecipe(recipe)}>+ Add</button>
-		</section>
-	);
+  return (
+    <section className='unlocked-card'>
+      <img src={recipe.image} alt={recipe.name} className='unlocked-image' />
+      <h3>{recipe.name}</h3>
+      <ul className='ingredients-list'>
+        {recipe.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <button onClick={() => onAddRecipe(recipe)}>+ Add</button>
+    </section>
+  );
 }
 
 export default CardsUnlocked;
