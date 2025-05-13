46c9a485b77ad167fa5daa78660294b5b98df554
Azuki19 <analucia27259@gmail.com>
2025-05-12 23:36:09 -0500

    feat: store edit

--- Foraneo-App/src/redux/store.js
+++ Foraneo-App/src/redux/store.js
@@ -1,16 +1,16 @@
-// src/redux/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import registerReducer from './signIn/registerSlice'; // Reducer para el registro de usuario
-import financeReducer from './finances/financesSlice'; // Reducer para manejar las finanzas
 
-// Configuración del store de Redux
+import { configureStore } from '@reduxjs/toolkit';
+import registerReducer from './signIn/registerSlice';
+import financeReducer from './finances/financesSlice';
+
+
 export const store = configureStore({
-	reducer: {
-		register: registerReducer,
-		finances: financeReducer, // Correcto: finanzas debe usar el reducer de financeSlice
-	},
+  reducer: {
+    register: registerReducer,
+    finances: financeReducer,
+  },
 });
 
-// Exportar las acciones de los slices
+
 export { setUser, logoutUser } from './signIn/registerSlice';
-export { setTransactions, addTransaction, removeTransaction } from './finances/financesSlice'; // Exporta también las acciones de financeSlice
+export { setTransactions, addTransaction, removeTransaction } from './finances/financesSlice';
