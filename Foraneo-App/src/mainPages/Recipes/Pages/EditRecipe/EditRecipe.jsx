a71464cedabcad7f98abbd09ec8c88e4e45b0037
Azuki19 <analucia27259@gmail.com>
2025-05-13 14:18:10 -0500

    feat: editRecipe handle change to unlocked recipes

--- Foraneo-App/src/mainPages/Recipes/Pages/EditRecipe/EditRecipe.jsx
+++ Foraneo-App/src/mainPages/Recipes/Pages/EditRecipe/EditRecipe.jsx
@@ -7,84 +7,91 @@ import Menu from '../../../Planner/components/navBar/navBar';
 import './EditRecipe.css';
 
 function EditRecipe() {
-	const navigate = useNavigate();
-	const [recipes, setRecipes] = useState(null);
-	const { day } = useParams();
+  const navigate = useNavigate();
+  const [recipes, setRecipes] = useState(null);
+  const { day } = useParams();
 
-	useEffect(() => {
-		const storedPlans = JSON.parse(localStorage.getItem('weeklyPlan'));
+  useEffect(() => {
+    const storedPlans = JSON.parse(localStorage.getItem('weeklyPlan'));
 
-		if (storedPlans) {
-			const selectedDayPlan = storedPlans.find((plan) => plan.day === day);
+    if (storedPlans) {
+      const selectedDayPlan = storedPlans.find((plan) => plan.day === day);
 
-			if (selectedDayPlan) {
-				setRecipes(selectedDayPlan);
-			}
-		}
-	}, [day]);
+      if (selectedDayPlan) {
+        setRecipes(selectedDayPlan);
+      }
+    }
+  }, [day]);
 
-	const handleGoBack = () => {
-		navigate('/weeklyplan');
-	};
+  const handleGoBack = () => {
+    navigate('/weeklyplan');
+  };
 
-	if (!recipes) return <div>Loading...</div>;
+  if (!recipes) return <div>Loading...</div>;
 
-	const getIngredients = (recipe) => {
-		if (recipe.ingredients) return recipe.ingredients;
-		return [];
-	};
+  const getIngredients = (recipe) => {
+    if (recipe.ingredients) return recipe.ingredients;
+    return [];
+  };
 
-	return (
-		<section className='bigContainerEditrecipe'>
-			<section className='button-chevro' onClick={handleGoBack}>
-				<ChevronLeft />
-			</section>
+  const handleEditRecipe = (mealTime) => {
+    navigate(`/unlockedrecipes/${day}/${mealTime}`);
+  };
 
-			<TitleEditRecipes day={day} />
+  return (
+    <section className='bigContainerEditrecipe'>
+      <section className='button-chevro' onClick={handleGoBack}>
+        <ChevronLeft />
+      </section>
 
-			{recipes.breakfast && (
-				<CardsEditRecipes
-					mealTime='Breakfast'
-					recipe={{
-						name: recipes.breakfast.name,
-						description: recipes.breakfast.description || 'Perfect to start your day!',
-						image: recipes.breakfast.image || 'default-breakfast.jpg',
-						ingredients: getIngredients(recipes.breakfast),
-					}}
-					day={day}
-				/>
-			)}
-			{recipes.lunch && (
-				<CardsEditRecipes
-					mealTime='Lunch'
-					recipe={{
-						name: recipes.lunch.name,
-						description: recipes.lunch.description || 'Energize your afternoon!',
-						image: recipes.lunch.image || 'default-lunch.jpg',
-						ingredients: getIngredients(recipes.lunch),
-					}}
-					day={day}
-				/>
-			)}
-			{recipes.dinner && (
-				<CardsEditRecipes
-					mealTime='Dinner'
-					recipe={{
-						name: recipes.dinner.name,
-						description: recipes.dinner.description || 'End your day deliciously!',
-						image: recipes.dinner.image || 'default-dinner.jpg',
-						ingredients: getIngredients(recipes.dinner),
-					}}
-					day={day}
-				/>
-			)}
+      <TitleEditRecipes day={day} />
 
-				<section className='spaceiwi'></section>
-			<section className='menucontaiiner'>
-			<Menu></Menu>
-			</section>
-		</section>
-	);
+      {recipes.breakfast && (
+        <CardsEditRecipes
+          mealTime='Breakfast'
+          recipe={{
+            name: recipes.breakfast.name,
+            description: recipes.breakfast.description || 'Perfect to start your day!',
+            image: recipes.breakfast.image || 'default-breakfast.jpg',
+            ingredients: getIngredients(recipes.breakfast),
+          }}
+          day={day}
+          onEditRecipe={() => handleEditRecipe('Breakfast')}
+        />
+      )}
+      {recipes.lunch && (
+        <CardsEditRecipes
+          mealTime='Lunch'
+          recipe={{
+            name: recipes.lunch.name,
+            description: recipes.lunch.description || 'Energize your afternoon!',
+            image: recipes.lunch.image || 'default-lunch.jpg',
+            ingredients: getIngredients(recipes.lunch),
+          }}
+          day={day}
+          onEditRecipe={() => handleEditRecipe('Lunch')}
+        />
+      )}
+      {recipes.dinner && (
+        <CardsEditRecipes
+          mealTime='Dinner'
+          recipe={{
+            name: recipes.dinner.name,
+            description: recipes.dinner.description || 'End your day deliciously!',
+            image: recipes.dinner.image || 'default-dinner.jpg',
+            ingredients: getIngredients(recipes.dinner),
+          }}
+          day={day}
+          onEditRecipe={() => handleEditRecipe('Dinner')}
+        />
+      )}
+
+      <section className='spaceiwi'></section>
+      <section className='menucontaiiner'>
+        <Menu />
+      </section>
+    </section>
+  );
 }
 
 export default EditRecipe;
