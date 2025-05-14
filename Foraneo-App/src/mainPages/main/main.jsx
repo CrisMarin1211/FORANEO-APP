c503f644ac04b82f469235f156e9367948378100
Azuki19 <analucia27259@gmail.com>
2025-05-14 15:14:24 -0500

    feat: added Image to main

--- Foraneo-App/src/mainPages/main/main.jsx
+++ Foraneo-App/src/mainPages/main/main.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './main.css';
 import Menu from '../Planner/components/navBar/navBar';
+import ForaneoImage from './components/ForaneoImage/ForaneoImage';
 
 
 const Main = () => {
 	return (
-		<section className='MainContainerPage'>
-			<h1>Main</h1>
 
+		<section className='MainContainerPage'>
+			<ForaneoImage></ForaneoImage>
+		<section className='MainContainerr'>
+			<h1>Hola, Foraneo</h1>
+			</section>
+			<section className='MenuBarr'>
 			<Menu></Menu>
+		</section>
 		</section>
 	);
 };
