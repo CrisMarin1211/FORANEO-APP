aa1d1fce7142bee359fd3c84f97158e11d423522
Azuki19 <analucia27259@gmail.com>
2025-05-14 15:16:23 -0500

    feat: correct use in cardImage

--- Foraneo-App/src/mainPages/Recipes/Components/6.DetailsRecipes/CardImageRecipes/CardImageRecipes.jsx
+++ Foraneo-App/src/mainPages/Recipes/Components/6.DetailsRecipes/CardImageRecipes/CardImageRecipes.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import './CardImageRecipes.css';
+import React from "react";
+import "./CardImageRecipes.css";
 
 function CardImageRecipes({ name, image, description }) {
-	return (
-		<section>
-			<h2 className='recipe-boxtitle'>{name}</h2>
-			<section className='image-wrap'>
-				<img src={image} alt={name} className='image-display' />
-			</section>
-			<p className='description-paragraph'>{description}</p>
-		</section>
-	);
+
+  return (
+    <section>
+      <h2 className="recipe-boxtitle">{name}</h2>
+      <section className="image-wrap">
+        {image ? (
+          <img src={image} alt={name} className="image-display" />
+        ) : null}
+      </section>
+      <p className="description-paragraph">{description}</p>
+    </section>
+  );
 }
 
 export default CardImageRecipes;
