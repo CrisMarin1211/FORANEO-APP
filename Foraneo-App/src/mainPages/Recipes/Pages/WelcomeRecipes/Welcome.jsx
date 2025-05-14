4ac34703b71fa87b061cf6a8c0051e65c51556d2
Azuki19 <analucia27259@gmail.com>
2025-05-14 15:18:30 -0500

    feat: welcome page if the user saved data

--- Foraneo-App/src/mainPages/Recipes/Pages/WelcomeRecipes/Welcome.jsx
+++ Foraneo-App/src/mainPages/Recipes/Pages/WelcomeRecipes/Welcome.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import './Welcome.css';
-import WelcomeNoData from '../../Components/Welcome Component/welcomeComp';
-import Menu from '../../../Planner/components/navBar/navBar';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import "./Welcome.css";
+import WelcomeNoData from "../../Components/Welcome Component/welcomeComp";
+import Menu from "../../../Planner/components/navBar/navBar";
 
 function Welcome() {
+  const weeklyPlan = useSelector((state) => state.recipes.weeklyPlan);
+  const navigate = useNavigate();
 
-	return (
-		<section className='welcomeContainer'>
-		<WelcomeNoData />
+  React.useEffect(() => {
+    if (weeklyPlan && weeklyPlan.length > 0) {
+      navigate("/weeklyplan");
+    }
+  }, [weeklyPlan, navigate]);
 
-		<Menu className='menusection'></Menu>
-		</section>
-	);
+  return (
+    <section className="welcomeContainer">
+      <WelcomeNoData />
+      <Menu className="menusection"></Menu>
+    </section>
+  );
 }
 
 export default Welcome;
