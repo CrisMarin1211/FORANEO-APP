7488e5cdfef92cacf103d512483af372686e3edf
Azuki19 <analucia27259@gmail.com>
2025-05-15 15:09:50 -0500

    style: sing In css

--- Foraneo-App/src/mainPages/signIn/signin.jsx
+++ Foraneo-App/src/mainPages/signIn/signin.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import SignInForm from './components/signInForm/signInForm';
+import './signin.css'
 
 const SignIn = () => {
 	return (
-		<section>
-			<h1>Sign In</h1>
+		<section className='ContainerSignIn'>
+			<h1 className='welcomeMessageSignIn'>Welcome to Foraneo Day!✨</h1>
+		<section className='signIn'>
+			<h1 className='welcomeMessageLogin1'>Sign Up Here!</h1>
 			<SignInForm />
+		</section>
 		</section>
 	);
 };
