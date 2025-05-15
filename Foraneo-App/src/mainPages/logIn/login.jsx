ef31855222cfe920d1d3e15ffa697e00b01ef5f1
Azuki19 <analucia27259@gmail.com>
2025-05-15 15:10:27 -0500

    feat: login logic with firebase

--- Foraneo-App/src/mainPages/logIn/login.jsx
+++ Foraneo-App/src/mainPages/logIn/login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom'; // Asegúrate de importar Link aquí
+import { Link } from 'react-router-dom';
 import LogInForm from './components/logInForm/logInForm';
 import './login.css';
 
@@ -9,7 +9,7 @@ const Login = () => {
 
   const onFinish = (user) => {
     if (user) {
-      
+
       navigate('/main');
     }
   };
@@ -19,12 +19,17 @@ const Login = () => {
   };
 
   return (
+    <section className='ContainerLoginn'>
+
+<h1 className='welcomeMessageLogin'>Welcome back to Foraneo Day!☀️</h1>
+
     <section className="login">
-      <h1>Login</h1>
+      <h1 className='welcomeMessageLogin1'>Login Here!</h1>
       <LogInForm onFinish={onFinish} onFinishFailed={onFinishFailed} />
-      <p style={{ textAlign: 'center' }}>
-        Don't have an account? <Link to="/">Sign up</Link>
+      <p className='plogin' style={{ textAlign: 'center' }}>
+        Don't have an account? <Link className='linkLogin' to="/">Sign up</Link>
       </p>
+    </section>
     </section>
   );
 };
