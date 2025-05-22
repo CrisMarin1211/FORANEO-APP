96711a720b7d2bb06fc781ee9e09dcd98b7cfbce
Azuki19 <analucia27259@gmail.com>
2025-05-22 18:13:58 -0500

    fix: add css import

--- src/mainPages/finances/pages/add/add.jsx
+++ src/mainPages/finances/pages/add/add.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Add.css';
+import './add.css';
 import AddSection from '../../../finances/components/addSection/addSection';
 import ValueInput from '../../components/valueInput/valueInput';
 import { ChevronLeft } from 'lucide-react';
 import Menu from '../../../Planner/components/navBar/navBar';
 
 const Add = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  return (
-    <section className='Addcontainer'>
+	return (
+		<section className='Addcontainer'>
+			<ChevronLeft className='backIcon' onClick={() => navigate('/finances')} />
 
-        <ChevronLeft className='backIcon' onClick={() => navigate('/finances')}/>
-
-      <AddSection />
-      <section className='spaceiwi'></section>
-      <Menu></Menu>
-
-    </section>
-  );
+			<AddSection />
+			<section className='spaceiwi'></section>
+			<Menu></Menu>
+		</section>
+	);
 };
 
 export default Add;
