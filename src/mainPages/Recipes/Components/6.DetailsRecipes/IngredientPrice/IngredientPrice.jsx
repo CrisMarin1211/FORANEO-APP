3284f4ab01825866308844afe8c2a39a480eaa4d
Azuki19 <analucia27259@gmail.com>
2025-05-22 18:15:27 -0500

    fix: ingredient price css import

--- src/mainPages/Recipes/Components/6.DetailsRecipes/IngredientPrice/IngredientPrice.jsx
+++ src/mainPages/Recipes/Components/6.DetailsRecipes/IngredientPrice/IngredientPrice.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./IngredientPrice.css";
+import "./ingredientPrice.css";
 import { updateDoc, doc, arrayUnion } from "firebase/firestore";
 import { auth, db } from "../../../../../services/firebaseConfig";
 
