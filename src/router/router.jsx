55ac1bc39b857cd3f3f6b15a8ba509b0886b301b
Azuki19 <analucia27259@gmail.com>
2025-05-22 18:10:14 -0500

    fix: ProtectedRoute Syntax Error

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { WelcomeView, PlanCreateView, WeeklyPlanView, EditRecipeView, UnlockedRecipesView, RecipeDetailView, CongratulationView } from '../mainPages/export';
-import { SignInView, LogInView, MainView, FinancesView, AddView, EmotionTrackerView, CalendarPageView, ToDoPageView, ProfileView, StepsRecipeView } from '../mainPages/export';
-import ProtectedRoute from '../mainPages/ProtectedRoute.jsX';
+import { SignInView, LogInView, MainView, FinancesView, AddView, EtmotionTrackerView, CalendarPageView, ToDoPageView, ProfileView, StepsRecipeView } from '../mainPages/export';
+import ProtectedRoute from '../mainPages/ProtectedRoute.jsx';
 
 
 
